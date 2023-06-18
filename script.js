5e89afbb459beaef40d44794e3f7e488800bc863
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-18 23:52:58 +0500

    Write script for the closing icon in popup window

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let menu = document.querySelector('.menu');
 menu.addEventListener('click', function() {
-  let popup = document.querySelector('.popup_menu');
+  let popup = document.querySelector('.popup-menu');
+  popup.classList.toggle('visible');
+});
+
+let close_popup = document.querySelector('.popup-close');
+close_popup.addEventListener('click', function() {
+  let popup = document.querySelector('.popup-menu');
   popup.classList.toggle('visible');
 });
