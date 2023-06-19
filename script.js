d128a70752a3285d946ea3fd6ba25595d63ce7f2
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-19 23:48:59 +0500

    Fix eslint error

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ closePopup.addEventListener('click', () => {
 });
 
 const navigateToSection = document.querySelectorAll('.popup-list a');
-for (let i = 0; i < navigateToSection.length; i++) {
+for (let i = 0; i < navigateToSection.length; i += 1) {
   navigateToSection[i].addEventListener('click', () => {
     const popup = document.querySelector('.popup-menu');
     popup.classList.toggle('visible');
